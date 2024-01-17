a72989094eb3ec7e9a4263a245f88ff3226f8844
paul-r <paulrousseaucs@gmail.com>
2024-01-18 00:12:24 +0100

    (FIX): Change color of bilan text

--- src/components/quizElements/quizBilan/QuizBilan.tsx
+++ src/components/quizElements/quizBilan/QuizBilan.tsx
@@ -21,7 +21,7 @@ const QuizBilan = ({
 }: Props) => {
   return (
     <div className="flex flex-col mt-5 items-center text-center [font-family:'Inter-Bold', Helvetica] space-y-10">
-      <h1 className="font-bold text-xl">Bilan de la session</h1>
+      <h1 className="text-[#4B4B4B] font-bold text-xl">Bilan de la session</h1>
       <ThemeCard themeName={themeName} themeLogo={themeLogo} />
       <div className="flex flex-row lg:space-x-48 md:space-x-24 space-x-5">
         <BilanCard
