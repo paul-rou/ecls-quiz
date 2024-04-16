ffca5df8beecb7db5feb571c6c48012bbd322fad
Cyprien Pierrot <cyprien.pierrot@student-cs.fr>
2024-04-16 17:49:49 +0200

    fix : improve loading

---
+++ src/lib/loadingTips.ts
@@ -0,0 +1,18 @@
+export const loadingTips: string[] = [
+    "Vous pouvez obtenir le badge correspondant à un thème en complétant avec succès tous les niveaux du thème",
+    "Environ 90% des femmes dans le monde ont subi du harcèlement sexuel dans les lieux publics, selon une étude de l'Union européenne.",
+    "Aux États-Unis, les femmes gagnent en moyenne 82 cents pour chaque dollar gagné par les hommes, selon le Bureau of Labor Statistics.",
+    "En France, près de 80% des femmes ont été confrontées à au moins une forme de sexisme au cours de leur vie professionnelle, selon une enquête du Haut Conseil à l'Égalité.",
+    "Une étude de l'UNESCO révèle que seulement 30% des chercheurs dans le monde sont des femmes, ce qui témoigne d'une sous-représentation importante dans ce domaine.",
+    "Près de 40% des femmes dans le monde ont subi de la violence physique ou sexuelle de la part d'un partenaire intime, selon l'Organisation mondiale de la santé.",
+    "Environ 1 femme sur 3 dans le monde a été victime de violence physique ou sexuelle perpétrée par un tiers ou par un partenaire intime, selon l'ONU Femmes.",
+    "Seulement 24% des membres des parlements nationaux dans le monde sont des femmes, selon l'Union interparlementaire.",
+    "Selon une étude de l'Université de Georgetown, les femmes occupent moins de 25% des postes de direction dans les entreprises du monde entier.",
+    "Environ 25% des femmes dans le monde ont subi des violences physiques ou sexuelles pendant la grossesse, selon une étude de l'Organisation mondiale de la santé.",
+    "Dans les médias, seulement 24% des personnes interrogées, citées, ou dont il est question sont des femmes, selon l'UNESCO.",
+    "Une enquête de l'Institut européen pour l'égalité entre les hommes et les femmes indique que 55% des femmes en Europe ont été confrontées à une forme de harcèlement sexuel.",
+    "Aux États-Unis, une femme est agressée sexuellement toutes les 73 secondes, selon le Rape, Abuse & Incest National Network (RAINN).",
+    "En moyenne, les femmes consacrent 2,5 fois plus de temps que les hommes aux tâches domestiques non rémunérées, selon l'Organisation internationale du travail.",
+    "Seulement 19% des articles de journaux sont écrits par des femmes, selon le Global Media Monitoring Project.",
+    "Une étude de l'Institut de statistique de l'UNESCO montre que moins de 30% des chercheurs sont des femmes dans plus des deux tiers des pays du monde."
+]
